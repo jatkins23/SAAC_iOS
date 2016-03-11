f1cb8d6eef675b1f776a9dc1c8898853bb2c333b
Jon Atkins <jon.atkins23@gmail.com>
2016-03-11 12:58:43 -0500

    initial commit

---
+++ SAAC_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAAC_iOS
+//
+//  Created by Jon Atkins on 3/11/16.
+//  Copyright © 2016 Tufts University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
