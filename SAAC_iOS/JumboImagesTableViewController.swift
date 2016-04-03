8bff20aa4948ff743f48095b92e802c8711760c5
Jon Atkins <jon.atkins23@gmail.com>
2016-04-03 19:31:14 -0400

    added JumboImagesTableViewController

---
+++ SAAC_iOS/JumboImagesTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  JumboImagesTableViewController.swift
+//  Jumbo_App
+//
+//  Created by Ryan Osgood on 3/11/16.
+//  Copyright © 2016 Ryan Osgood. All rights reserved.
+//
+
+import UIKit
+
+class JumboImagesTableViewController: UITableViewController {
+    
+    var cells = [JumboImages]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    func loadSampleData() {
+        let sample = JumboImages(UserName: "Linus Gordon", UserImage: UIImage(named:"kid_playing")!, TimeStamp:21, Likes: 57, EventImage: UIImage(named:"kid_playing")!, ImageCaption: "Playing Soccer!")!
+        
+        cells += [sample]
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "JumboImageViewCell"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! JumboImagesTableViewCell
+        
+        let JumboImage = cells[indexPath.row]
+        
+        cell.UserName.text = JumboImage.UserName
+        cell.UserImage.image = JumboImage.UserImage
+        //cell.Likes.setValue(JumboImage.Likes, forKey: <#T##String#>)
+        //cell.TimeStamp.setValue(JumboImage.TimeStamp, forKey: <#T##String#>)
+        cell.EventImage.image = JumboImage.EventImage
+        cell.ImageCaption.text = JumboImage.ImageCaption
+        
+        return cell
+    }
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
