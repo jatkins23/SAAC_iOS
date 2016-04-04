bab5f336f6b479de2ed4021fddbb882e4fad13e8
Jon Atkins <jon.atkins23@gmail.com>
2016-04-03 22:21:26 -0400

    added Leaderboard and Calendar classes

---
+++ SAAC_iOS/LeaderboardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaderboardTableViewCell.swift
+//  SAAC_iOS
+//
+//  Created by Jon Atkins on 4/3/16.
+//  Copyright © 2016 Tufts University. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
